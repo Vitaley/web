71b01c72364eb37c573772f6d304b0583fbf10bf
Vitaley <48580555+Vitaley@users.noreply.github.com>
2019-08-18 10:39:33 +0500

    test
    test

--- etc/gunicorn_cfg.py
+++
@@ -1,8 +0,0 @@
-mode = 'wsgi'
-working_dir = '/home/box/web'
-python = '/usr/bin/python3'
-bind = '0.0.0.0:8080'
-workers = 4
-timeout = 60
-log-level = 'debug'
-#'hello:app', # application for unicorn
--- init.sh
+++ init.sh
@@ -11,9 +11,10 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo /etc/init.d/nginx restart
 
 # make hrdlink for unicorn config and restart gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn_cfg.py /etc/gunicorn.d/gunicorn_cfg.py
+sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
 #sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 #sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart -c /etc/gunicorn.d/gunicorn_cfg.py hello:app
+cd web
+sudo gunicorn restart -c /etc/gunicorn.d/gunicorn.py hello:app
 #gunicorn -b 0.0.0.0:8080 hello:app_me&
 #sudo /etc/init.d/mysql start
